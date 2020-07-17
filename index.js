0ce9de1caf3f2f8c9a5c9b28111feb05fb823775
Sushmitha BS <bssushmitha26@gmail.com>
2020-07-17 14:31:54 +0200

    added register and login routes for teacher model

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 const db = require('./db')
-const teacherModel = require('./teachers/model')
+const bodyParser = require('body-parser')
+const teacherRouter = require('./teacher/router')
+
+//routes
+app.use(bodyParser.json())
+app.use(teacherRouter)
 
 const port = process.env.PORT
-
 app.listen(port, () => console.log('Server up and running'))
