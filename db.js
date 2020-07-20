c8a0606e6ad1cb67f5497cb7ccbe33a5fdbe9849
Sushmitha BS <bssushmitha26@gmail.com>
2020-07-20 12:04:51 +0200

    restary database (removing force: true)

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseUrl = process.env.DATABASE_URL
 const db = new Sequelize(databaseUrl)
 
 db
-    .sync({ force: true })
+    .sync()
     .then(() => console.log('Database connected'))
     .catch(err => console.log(err))
 
